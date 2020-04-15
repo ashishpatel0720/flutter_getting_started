49c4481aae69adaf8d6367de0eddf496e7b7dc89
ashish <ashishpatel0720@gmail.com>
2020-04-15 16:55:28 +0530

    module/ designing ui with widgets done

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,13 @@
-import 'package:flutter/material.dart'; // material design component for flutter
+import 'package:flutter/material.dart';
 import 'package:helloflutter/screens/home.dart';
 
-void main() => runApp( // create and attach widget to app
-    HelloWorldApp()
-  );
+void main(){
+  runApp(
+    MaterialApp(
+      title: "Pizza Ordering App",
+      home: Home(),
+    )
 
-class HelloWorldApp extends StatelessWidget { // stateless widget is a generic Widge
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp( // allows us to give App a name
-      debugShowCheckedModeBanner: false, // if want to remove 'debug' banner
-      title: "Hello Flutter App",
-      home: Scaffold (  // allows us to have basic material design layout
-        appBar: AppBar(
-            title: Text("App title bar")
-        ),
-        body: Home()
-      ),
-    );
-  }
+  );
 
 }
