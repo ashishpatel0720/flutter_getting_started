6b1420313f60de6363bd535a395df10268071411
ashish <ashishpatel0720@gmail.com>
2020-04-15 13:47:36 +0530

    [APP]: adding logic done

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -8,7 +8,7 @@ class Home extends StatelessWidget { // stateless widget is a generic Widge
         child:
         Center( // container widget which will center its child horizontally and vertically
             child: Text(
-              "Home\nHello World !",
+              sayHello(),
               style: TextStyle(color: Colors.white,fontSize: 36.0),
               textDirection: TextDirection.ltr, // left to right
             )
@@ -16,4 +16,22 @@ class Home extends StatelessWidget { // stateless widget is a generic Widge
     );
   }
 
+  String sayHello(){
+    String hello="hello";
+
+    int hour= DateTime.now().hour;
+    int minute=DateTime.now().minute;
+    int second=DateTime.now().second;
+
+    String time=hour.toString()+":"+minute.toString();
+
+    if(hour<12)
+      hello="Good Morning";
+    else if(hour<16)
+      hello="Good Afternoon";
+    else
+      hello="Good Evening";
+
+    return "Its now $time.\n"+hello;
+  }
 }
