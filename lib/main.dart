0f0c9e3de6d8b7565c027d00eac442ee59beacd9
ashish <ashishpatel0720@gmail.com>
2020-04-15 12:28:15 +0530

    [APP]: enhanced app with colors App bar title and application name

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,25 @@ import 'package:flutter/material.dart'; // material design component for flutter
 
 
 void main(){
-
   runApp( // create and attach widge to app
-    Center( // container widget which will center its child horizontally and vertically
-        child: Text(
-          "Hello World!",
-          textDirection: TextDirection.ltr, // left to right
-        )
-    )
+    MaterialApp( // allows us to give App a name
+      title: "Hello Flutter App",
+      home: Scaffold (  // allows us to have basic material design layout
+        appBar: AppBar(
+          title: Text("App title bar")
+        ),
+          body: Material( // new is optional
+              color: Colors.deepPurple,
+              child:
+              Center( // container widget which will center its child horizontally and vertically
+                  child: Text(
+                    "Hello World!",
+                    style: TextStyle(color: Colors.white,fontSize: 36.0),
+                    textDirection: TextDirection.ltr, // left to right
+                  )
+              )
+          ),
+        ),
+      ),
   );
 }
