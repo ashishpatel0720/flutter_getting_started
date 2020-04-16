e7287f6401cd35b8963b97641682b93888149e08
ashish <ashishpatel0720@gmail.com>
2020-04-16 19:03:02 +0530

    dropdown added with hello you

--- lib/main.dart
+++ lib/main.dart
@@ -18,8 +18,12 @@ class HelloYou extends StatefulWidget{
 
 class _HelloYouState extends State<HelloYou> {
   String name='';
+  final _currencies = ["Dollar","Euro","Pound"];
+  String _currency='Dollar'; // default value
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         title: Text("HelloYou App"),
@@ -31,7 +35,7 @@ class _HelloYouState extends State<HelloYou> {
           children: <Widget>[
             TextField(
               decoration: InputDecoration(
-                  helperText:"Please input your name"
+                  hintText:"Please input your name"
               ),
               onChanged: (String str){
                 setState(() { // only setState() will be able to change state ( same as React)
@@ -39,7 +43,22 @@ class _HelloYouState extends State<HelloYou> {
                 });
               },
             ),
-           Text("Hello "+ name+ '!')
+            Text("Hello "+ name+ '!'),
+
+            DropdownButton<String>(
+              items: _currencies.map((String value ){
+                return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value)
+                );
+              }).toList(),
+              value: _currency,
+              onChanged: (String value){
+                setState(() {
+                  this._currency= value;
+                });
+              },
+            )
           ],
         ),
       ),
