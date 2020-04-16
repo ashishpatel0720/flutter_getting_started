fa712a40859704cbe02f009f00000b267ae57ee1
ashish <ashishpatel0720@gmail.com>
2020-04-16 16:20:58 +0530

    Hello You APp created

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,49 @@
 import 'package:flutter/material.dart';
-import 'package:helloflutter/screens/home.dart';
 
 void main(){
   runApp(
-    MaterialApp(
-      title: "Pizza Ordering App",
-      home: Home(),
-    )
+      MaterialApp(
+        title: "Pizza Ordering App",
+        home: HelloYou(),
+      )
 
   );
+
+}
+
+class HelloYou extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => _HelloYouState();
+}
+
+class _HelloYouState extends State<HelloYou> {
+  String name='';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("HelloYou App"),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(15.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(
+                  helperText:"Please input your name"
+              ),
+              onChanged: (String str){
+                setState(() { // only setState() will be able to change state ( same as React)
+                  name = str;
+                });
+              },
+            ),
+           Text("Hello "+ name+ '!')
+          ],
+        ),
+      ),
+    );
+  }
 
 }
